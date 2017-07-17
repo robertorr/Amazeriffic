f9e63f38aafedc29643be5eb84240f1daa464ffa
Robert Orr <Rob@RobertOrr.net>
2017-07-17 19:51:35 -0400

    Added tags and handling

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,26 @@
 
 var organizeByTag = function (todoObjects) {
     "use strict";
-    return null;
+
+    // loop over all tasks and create list of tags
+    var tags = [];
+    todoObjects.forEach(function (todoObj) {
+        todoObj.tags.forEach(function (tag) {
+            if (tags.indexOf(tag) === -1) {
+                tags.push(tag);
+            }
+        });
+    });
+
+    return tags.map(function (tag) {
+        var taggedTodos = [];
+        todoObjects.forEach(function (todoObj) {
+            if (todoObj.tags.indexOf(tag) !== -1) {
+                taggedTodos.push(todoObj.description);
+            }
+        });
+        return {"name": tag, "todos": taggedTodos};
+    });
 };
 
 var main = function (todoObjs) {
@@ -15,16 +34,23 @@ var main = function (todoObjs) {
     const buttonSelector = mainSelector + " button";
     const tabsSelector = ".tabs a span";
 
-    var todos = todoObjs.map(function (todo) {
-        return todo.description;
-    });
+    var todos = [];
+    var updateTodos = function (todoObjects) {
+        todos = todoObjects.map(function (todo) {
+            return todo.description;
+        });
+    };
     var addNewTodo = function () {
-        if ($(inputSelector).val() !== "") {
-            todos.push($(inputSelector).val());
+        var description = $(inputSelector + ".description").val();
+        if (description !== "") {
+            var tags = $(inputSelector + ".tags").val().split(",");
+            todoObjs.push({"description": description, "tags": tags});
+            updateTodos(todoObjs);
             $(inputSelector).val("");
         }
     };
 
+    updateTodos(todoObjs);
     $(tabsSelector).toArray().forEach(function (element) {
         $(element).on("click", function () {
 
@@ -49,8 +75,6 @@ var main = function (todoObjs) {
                 });
                 $(mainSelector).append(jqContent);
             } else if (jqElement.parent().is(":nth-child(3)")) {
-                console.log("tags tab clicked");
-
                 var todosByTags = organizeByTag(todoObjs);
                 todosByTags.forEach(function (tag) {
                     var jqTagName = $("<h3>").text(tag.name);
@@ -59,12 +83,24 @@ var main = function (todoObjs) {
                         var jqli = $("<li>").text(description);
                         jqContent.append(jqli);
                     });
-                    $("main .content").append(jqTagName);
-                    $("main .content").append(jqContent);
+                    $(mainSelector).append(jqTagName);
+                    $(mainSelector).append(jqContent);
+                    $(mainSelector).append($("<br>"));
                 });
-
             } else if (jqElement.parent().is(":nth-child(4)")) {
-                jqContent = $("<input type=\"text\" /><button>+</button>");
+                jqContent = $("<h3>").text("Description (required):");
+                $(mainSelector).append(jqContent);
+                jqContent = $("<input type=\"text\" />").addClass("description");
+                $(mainSelector).append(jqContent);
+                $(mainSelector).append($("<br>"));
+                $(mainSelector).append($("<br>"));
+                jqContent = $("<h3>").text("Tags (optional):");
+                $(mainSelector).append(jqContent);
+                jqContent = $("<input type=\"text\" />").addClass("tags");
+                $(mainSelector).append(jqContent);
+                $(mainSelector).append($("<br>"));
+                $(mainSelector).append($("<br>"));
+                jqContent = $("<button>Add Task</button>");
                 $(mainSelector).append(jqContent);
                 $(inputSelector).on("keypress", function (event) {
                     if (event.keyCode === 13) {
@@ -74,6 +110,7 @@ var main = function (todoObjs) {
                 $(buttonSelector).on("click", function () {
                     addNewTodo();
                 });
+                $(inputSelector + ".description").focus();
             }
             return false;
         });
